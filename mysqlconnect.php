0b49ecb5ea9b7289018d0458e0c01005196ed769
John <jnasseh7801@gmail.com>
2024-09-28 13:55:01 -0400

    added env sample file and adjusted connect code to use it

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,7 +1,14 @@
 #!/usr/bin/php
 <?php
 
-$mydb = new mysqli('192.168.194.225','dbconnect','IT490DBCONNECT225','IT490');
+$env = parse_ini_file('.env');
+
+$db_host = $env['DB_HOST'];
+$db_user = $env['DB_USER'];
+$db_password = $env['DB_PASS'];
+$db_name = $env['DB_NAME'];
+
+$mydb = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 if ($mydb->errno != 0)
 {
