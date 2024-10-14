3350f0e171db7b0981d06c057376ddb5805d89ae
John <jnasseh7801@gmail.com>
2024-10-13 23:24:05 -0400

    login functionality

--- login.php
+++ login.php
@@ -1,75 +1,77 @@
-#!/usr/bin/php
 <?php
-session_start();
-
-require_once('mysqlconnect.php');
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+require_once('vendor/autoload.php');
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+$env = parse_ini_file('.env');
+$jwt_secret = $env['JWT_SECRET'];
+
+// rabbitmq client
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
-if ($mydb->connect_error) {
-    die("Connection failed: " . $mydb->connect_error);
-}
 
+// check if post
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    $msg = "No POST request made";
+    $msg = ["status" => "error", "message" => "No POST request made"];
     echo json_encode($msg);
     exit(0);
 }
 
-if (!isset($_POST['type'])) {
-    $msg = "Missing parameters";
+// check if username and password fields are in the POST request
+if (!isset($_POST['uname']) || !isset($_POST['pword'])) {
+    error_log("LOgin request missing username or password");
+    $msg = ["status" => "error", "message" => "Missing username or password"];
     echo json_encode($msg);
     exit(0);
+} else {
+	// log received username and password for debugging
+	error_log("username: " . $_POST['uname']);
+	error_log("password: " . $_POST['pword']);
 }
 
-$request = $_POST;
-$response = "Unsupported request type";
+// getting data from the login form and saniziting
+$username = htmlspecialchars(strip_tags($_POST['uname']));
+$password = htmlspecialchars(strip_tags($_POST['pword']));
 
-switch ($request["type"]) {
-    case "login":
-        if (!isset($request['uname']) || !isset($request['pword'])) {
-            $response = "Missing username or password.";
-            break;
-        }
-	$rabbitRequest = array();
-	$rabbitRequest['type'] = 'login';
-	$rabbitRequest['username'] = $request['uname'];
-	$rabbitRequest['password'] = $request['pword'];
+// creating and sending request to rabbitmq
+$rabbitRequest = array();
+$rabbitRequest['type'] = 'login';
+$rabbitRequest['username'] = $username;
+$rabbitRequest['password'] = $password;
 
-	// sending requests to rabbitmq through queue
-	$response = $client->send_request($rabbitRequest, "auth_responses");
+// sending login request to rabbitmq
+$response = $client->send_request($rabbitRequest, "auth_responses");
 
-        if ($response['status'] == 'success') {
-                $_SESSION['loggedin'] = true;
-                $_SESSION['username'] = $response['username'];
-                $_SESSION['email'] = $response['email'];
-                $_SESSION['created'] = $response['created'];
-                $response = "Login successful! Welcome, " . $response['username'] . ".";
-            } else {
-                $response = "Login failed. Incorrect password.";
-            }
-        break;
+// rabbitmq response handling
+if ($response['status'] == 'success') {
 
-    case "logout":
-        if (isset($_SESSION['loggedin'])) {
-            session_unset();  
-            session_destroy();  
-            $response = "Logout successful. See you next time!";
-        } else {
-            $response = "You are not logged in.";
-        }
-        break;
+	$payload = [
+		'iss' => 'IT490', // isuer
+		'iat' => time(), // issued at
+		'exp' => time() + 3600, //expiration time
+		'data' => [
+			'username' => $response['username'], // username in payload
+			'email' => $response['email'] // email in payload
+		]
+	];
 
-    default:
-        $response = "Unsupported request type";
-        break;
+	// encoding payload to generate jwt with secret key and  hs256 alg
+	$jwt = JWT::encode($payload, $jwt_secret, 'HS256');
+
+	// returning success message with generated jwt
+    echo json_encode(["status" => "success", "message" => "Login successful! Welcome, " . $response['username'] . ".", "token" => $jwt]);
+} else {
+    echo json_encode(["status" => "fail", "message" => $response['message']]);
 }
 
-
-echo json_encode($response);
 exit(0);
 ?>
 
