1e60f0de946659eea8256deb51801666d40c3180
Matt0810 <98350993+Matt0810@users.noreply.github.com>
2024-11-26 21:36:25 -0500

    changed queue

--- install_handler.php
+++ install_handler.php
@@ -48,6 +48,6 @@ function requestProcessor($request) {
     return ["status" => "error", "message" => "Invalid request type"];
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini", "deploymentMQ");
+$server = new rabbitMQServer("testRabbitMQ.ini", "installMQ");
 $server->process_requests('requestProcessor');
 ?>
