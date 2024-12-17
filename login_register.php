c0bd6fd5c1b3935cb800eda93aaa94dbcecf5b93
John <jnasseh7801@gmail.com>
2024-12-16 21:07:33 -0500

    reverted changes

--- login_register.php
+++ login_register.php
@@ -16,8 +16,6 @@ use Firebase\JWT\Key;
 $env = parse_ini_file('.env');
 $mydb = getDB();
 $jwt_key = $env['JWT_SECRET']; 
-$triggerMachine = $_SERVER['HTTP_X_ORIGIN_MACHINE'] ?? 'UnknownMachine';
-$triggerHost = $_SERVER['HTTP_X_ORIGIN_HOST'] ?? 'UnknownHost';
 // register function: handles user registration
 function doRegister($username, $password, $email)
 {
@@ -32,7 +30,7 @@ function doRegister($username, $password, $email)
 
     if ($result->num_rows > 0) {
 	// logging error message
-	log_message("Registration failed: Username or email already exists. Username: $username, Email: $email", $triggerMachine, $triggerHost);    
+	log_message("Registration failed: Username or email already exists. Username: $username, Email: $email");    
     // return failure if username or email exists
         return ["status" => "fail", "message" => "Username or email already exists."];
     }
