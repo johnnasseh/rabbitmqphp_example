6cbe4c8866c7162b502c33118971c3bbaa62c5a7
Matt0810 <98350993+Matt0810@users.noreply.github.com>
2024-12-19 05:51:35 -0500

    Create pass_bundle.php

---
+++ pass_bundle.php
@@ -0,0 +1,24 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+if ($argc < 3) {
+    echo "Use: php pass_bundle.php (bundle_name) (version)\n";
+    exit(1);
+}
+
+$bundleName = $argv[1];
+$version = (int)$argv[2];
+
+$client = new rabbitMQClient("testRabbitMQ.ini", "markingMQ");
+$request = [
+    'type' => 'mark_bundle',
+    'bundle_name' => $bundleName,
+    'version' => $version,
+    'status' => 'passed'
+];
+
+$response = $client->send_request($request);
+echo "Response: " . print_r($response, true) . "\n";
+?>
